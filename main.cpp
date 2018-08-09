319904972fa65da0db7037db29833a2b531f883c
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-08-09 11:53:06 +0000

    UPDATE: included new menu text method

--- main.cpp
+++ main.cpp
@@ -6,9 +6,11 @@ void await_input(){
 using namespace std;
 
 int main(){
+	
+	show_menu_text();
 	bool exit_reponse;
 	do {
-		exit_reponse = main_menu();
+		exit_reponse = select_main_menu_option();
 	}while(exit_reponse == false);
 	
 	cout << "Thank you for playing!" << endl;
