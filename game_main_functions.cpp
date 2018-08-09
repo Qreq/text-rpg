ebcd31ec4504e7438d9dea21470260211e9a40a7
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-08-09 11:36:49 +0000

    UPDATE: Now properly converts string to lowercase

--- game_main_functions.cpp
+++ game_main_functions.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "tolower.cpp"
 
 
 using namespace std;
@@ -7,15 +8,15 @@ using namespace std;
 //displays main menu options to the player
 bool main_menu(){
 	string welcome_message = "Hello, and Welcome to *game name here*";
-	cout << welcome_message; << endl;
+	cout << welcome_message << endl;
 	cout << "Select an option:" << endl;
 	cout << "Play" << endl;
-	cout << "Exit" << endl;
-	cout << "~~~~~~~~~~~" < endl;
+	cout << "Quit" << endl;
+	cout << "~~~~~~~~~~~" << endl;
 	
 	string main_option;
 	getline(cin,main_option);
-	main_option.tolower();
+	main_option = convert_to_lower(main_option);
 	
 	if (main_option == "quit"){
 		return true;
