fda40577a1840f5c436862b6814c3fada817fca6
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-08-09 20:45:19 +0000

    UPDATE: can now name character in opening sequence

--- game_start.cpp
+++ game_start.cpp
@@ -2,7 +2,11 @@
 #include <iostream>
 #include "Character.hpp"
 
-
+/*
+ * 
+ * TODO: MUST FIND A WAY TO INCORPORATE RACE INTO OPENING SEQUENCE
+ * 
+ */
 using namespace std;
 
 //new game 
@@ -22,12 +26,13 @@ void opening_sequence(){
 	cout << "What will your gender be? (male/female)" << endl;
 	do{
 		cin >> gender;
+		cin.ignore();
 		gender = convert_to_lower(gender);
 		//TODO HERE: conditional to prompt user if they entered male or female
 	}while(gender != "male" && gender != "female");
 	
 	my_player.gender = gender;
-	
+	cout << "You notice that you are now in nothing more than a drab, baggy sack-like pelts from the dregs of the local city" << endl;
 	cout << "Your eyes fix upon the chiseled face in front of you, a smile plastered across it." << endl; 
 	if (my_player.gender == "female"){
 		my_player.possessive_pronoun = "her";
@@ -40,8 +45,23 @@ void opening_sequence(){
 		cout << "???: Oh, how boring.  Another delinquent.  At least this one is ripe." << endl;
 	}
 	
+	cout << "??: Just don't be too long, I can hear the Captain coming." << endl;
+	cout << "The grinning figure straightens up, allowing you to see more clearly who he is as your eyes come to focus." << endl;
+	cout << "The man dons what seems to be chainmail chest plate with steal boots and gauntlets to match." << endl;
+	cout <<	"Underneath all of the armour, stained cloth undergarments visibly stick to his legs.  He appears to have no helmet, allowing you to see his horrific face" << endl;
+	cout << "A wicked grin matched with wild eyes tower over you, fresh saliva coating over his flicking tongue." << endl;
+	cout << "He grabs a hold of your hair roughly yanking it backwards that causes you to muffle a pained moan" << endl;
+	cout << "You hear the noise of a door open behind you as spit graces your face and slithers down your cheek" << endl;
+	cout << "????:  Simmons, what DO you think you are you doing?  Did you NOT see this person's papers?" << endl;
+	cout << "You assume the man who just walked in is the Captain that was mentioned previously, and that he held a much higher rank than the Simmons character" << endl;
+	cout << "Simmons:  I didn't. I... must have misplaced it" << endl;
+	cout << "His grin starts to melt into the pout of a child wishing forgiveness." << endl''
+	cout << "The Captain:  Seems like you misplaced your brain as well.  Not like you could have read it anyway, even with one." << end;
+	cout << "Simmons backs away as the Captain strides to your side." << endl;
+	cout << "The Captain:  So sorry about the lug here.  Now remind me, what was your name?" << endl;
 	
-	
-	
-	
+	string name;
+	getline(cin,name);
+	player.name = name;
+
 }
