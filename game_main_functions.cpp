b82c5aded215ddd827e3d6f29b3351c3304e048c
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-08-09 11:52:26 +0000

    UPDATE: tweaked layout of main menu text

--- game_main_functions.cpp
+++ game_main_functions.cpp
@@ -5,15 +5,21 @@
 
 using namespace std;
 
-//displays main menu options to the player
-bool main_menu(){
-	string welcome_message = "Hello, and Welcome to *game name here*";
+//displays welcome message and menu options
+void show_menu_text(){
+	cout << "~~~~~~~~~~~~~~~" << endl;
+	string welcome_message = "Welcome to *game name here*";
 	cout << welcome_message << endl;
 	cout << "Select an option:" << endl;
 	cout << "Play" << endl;
 	cout << "Quit" << endl;
-	cout << "~~~~~~~~~~~" << endl;
+	cout << "~~~~~~~~~~~~~~~" << endl;
+}
+
+//handle what the player inputs
+bool select_main_menu_option(){
 	
+	//string to hold the text that the player enters
 	string main_option;
 	getline(cin,main_option);
 	main_option = convert_to_lower(main_option);
@@ -21,7 +27,11 @@ bool main_menu(){
 	if (main_option == "quit"){
 		return true;
 	}
+	//to start a new game
 	if (main_option == "play"){
 		//TODO: call function to start a new game
+	}
+	else{
+		cout << "Menu Choice Not Valid" << endl;
 	}
 }
