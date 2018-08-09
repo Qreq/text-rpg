369da2052c5467ec9410f980ff0b2f4a82676540
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-08-09 14:50:51 +0000

    added class file for characters

---
+++ Character.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include "Character.hpp"
+using namespace std;
+
+class Character{
+	
+	public:
+		string gender,subject_pronoun,possessive_pronoun,name; //subject = she/he, possessive = her/him
+	
+}
