bd6c0464daa56c812aaaffe83c604c4f56803bf2
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-08-09 20:21:18 +0000

    quit condition moved here, and bug fix with cin.ignore

--- main_menu.cpp
+++ main_menu.cpp
@@ -18,23 +18,26 @@ void show_menu_text(){
 }
 
 //handle what the player inputs
-bool select_main_menu_option(){
-	
-	//string to hold the text that the player enters
+void select_main_menu_option(){
 	string main_option;
-	getline(cin,main_option);
-	main_option = convert_to_lower(main_option);
-	
-	if (main_option == "quit"){
-		return true;
-	}
-	//to start a new game
-	if (main_option == "play"){
-		cout << "You wish to embark along the path of the underlings?  So be it..." << endl;
-		cout << "~~~~~~~~~~~~~~~~~" << endl;
-		opening_sequence();
-	}
-	else{
-		cout << "Menu Choice Not Valid" << endl;
-	}
+	do{
+		//string to hold the text that the player enters
+		
+		getline(cin,main_option);
+		main_option = convert_to_lower(main_option);
+		
+		if (main_option == "quit"){
+			return;
+		}
+		//to start a new game
+		if (main_option == "play"){
+			cout << "You wish to embark along the path of the underlings?  So be it..." << endl;
+			cout << "~~~~~~~~~~~~~~~~~" << endl;
+			//start opening sequence to game
+			opening_sequence();
+		}
+		else{
+			cout << "Menu Choice Not Valid" << endl;
+		}
+	}while(main_option != "quit");
 }
