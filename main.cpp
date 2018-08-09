6f63c9a6928a9db3d430dce6ece9e76ca58d98ae
Nathan Brown <brownn28@uni.coventry.ac.uk>
2018-08-09 20:20:36 +0000

    moved loop to main_menu.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,10 @@
 #include "main_menu.cpp"
 
 using namespace std;
-
 int main(){
 	
 	show_menu_text();
-	bool exit_reponse;
-	do {
-		exit_reponse = select_main_menu_option();
-	}while(exit_reponse == false);
+	select_main_menu_option();
 	
 	cout << "Thank you for playing!" << endl;
 	return 0;
